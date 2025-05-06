6ec5d09962610559c25f526b5396ff4d8395e6d2
AntosCichon <antekcichon@gmail.com>
2025-05-06 04:36:01 +0200

    v1
    First version, works for now

---
+++ main.go
@@ -0,0 +1,265 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/google/uuid"
+)
+
+type Entry struct {
+	Id       uuid.UUID `json:"id"`
+	Title    string    `json:"title"`
+	Modifier int       `json:"modifier"`
+	Color    string    `json:"color"`
+}
+
+func getEntries() ([]Entry, error) {
+	data, err := os.ReadFile("list.json")
+	if err != nil {
+		fmt.Printf("\033[31mCannot read file list.json\033[0m\n%s\n", err)
+		return nil, err
+	} else if len(data) < 2 {
+		// when file is less than 2 characters long, just ignore it and use empty json list: []
+		data = []byte("[]")
+	}
+	var entries []Entry
+	err = json.Unmarshal(data, &entries)
+	if err != nil {
+		fmt.Printf("\033[31mCannot Unmarshal json from list.json\033[0m\n%s\n", err)
+		return nil, err
+	}
+	return entries, nil
+}
+func saveEntry(entry Entry) error {
+	entries, err := getEntries()
+	if err != nil {
+		fmt.Printf("\033[31mCannot get Entries from file list.json033[0m\n%s\n", err)
+		return err
+	}
+
+	entries = append(entries, entry)
+
+	entriesJson, err := json.Marshal(entries)
+	if err != nil {
+		fmt.Printf("\033[31mCannot Marshal entries slice\033[0m\n%s\n", err)
+		return err
+	}
+
+	err = os.WriteFile("list.json", entriesJson, 0644)
+	if err != nil {
+		fmt.Printf("\033[31mCannot save file list.json\033[0m\n%s\n", err)
+		return err
+	}
+
+	return nil
+}
+func removeEntry(entries []Entry, id string) ([]Entry, error) {
+	deleteId, err := uuid.Parse(id)
+	if err != nil {
+		fmt.Printf("\033[31mCannot parse string uuid to uuid.UUID\033[0m\n%s\n", err)
+		return nil, err
+	}
+	newEntries := entries
+	for i, entry := range entries {
+		if entry.Id == deleteId {
+			newEntries = append(entries[:i], entries[i+1:]...)
+			break
+		}
+	}
+
+	entriesJson, err := json.Marshal(newEntries)
+	if err != nil {
+		fmt.Printf("\033[31mCannot Marshal entries slice\033[0m\n%s\n", err)
+		return nil, err
+	}
+
+	err = os.WriteFile("list.json", entriesJson, 0644)
+	if err != nil {
+		fmt.Printf("\033[31mCannot save file list.json\033[0m\n%s\n", err)
+		return nil, err
+	}
+
+	return newEntries, nil
+}
+func changeModifier(entries []Entry, id string, modifier int) ([]Entry, error) {
+	modifyId, err := uuid.Parse(id)
+	if err != nil {
+		fmt.Printf("\033[31mCannot parse string uuid to uuid.UUID\033[0m\n%s\n", err)
+		return nil, err
+	}
+	newEntries := entries
+	for i, entry := range entries {
+		if entry.Id == modifyId {
+			entries[i].Modifier = modifier
+			break
+		}
+	}
+
+	entriesJson, err := json.Marshal(newEntries)
+	if err != nil {
+		fmt.Printf("\033[31mCannot Marshal entries slice\033[0m\n%s\n", err)
+		return nil, err
+	}
+
+	err = os.WriteFile("list.json", entriesJson, 0644)
+	if err != nil {
+		fmt.Printf("\033[31mCannot save file list.json\033[0m\n%s\n", err)
+		return nil, err
+	}
+
+	return newEntries, nil
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	entries, err := getEntries()
+	if err != nil {
+		http.Error(w, "Failed to read ToDos file", http.StatusInternalServerError)
+		return
+	}
+	tmpl := template.Must(template.ParseFiles("static/index.html"))
+	tmpl.Execute(w, entries)
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Invalid POST data", http.StatusBadRequest)
+		return
+	}
+
+	entryTitle := r.FormValue("title")
+	if entryTitle == "" {
+		http.Error(w, "Field \"title\" cannot be empty", http.StatusBadRequest)
+		return
+	}
+	entryColor := r.FormValue("color")
+
+	entry := Entry{
+		Id:       uuid.New(),
+		Title:    html.EscapeString(entryTitle),
+		Modifier: 0,
+		Color:    entryColor,
+	}
+	err = saveEntry(entry)
+	if err != nil {
+		http.Error(w, "Failed to save new entry", http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(entry)
+	if err != nil {
+		http.Error(w, "Failed to Marshal response body", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.Write(response)
+}
+
+func removeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Invalid POST data", http.StatusBadRequest)
+		return
+	}
+
+	deleteId := r.FormValue("id")
+	if deleteId == "" {
+		http.Error(w, "Field \"id\" cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	entries, err := getEntries()
+	if err != nil {
+		http.Error(w, "Failed to read ToDos file", http.StatusInternalServerError)
+		return
+	}
+
+	newEntries, err := removeEntry(entries, deleteId)
+	if err != nil {
+		http.Error(w, "Failed to remove entry from file", http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(newEntries)
+	if err != nil {
+		http.Error(w, "Failed to Marshal response body", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.Write(response)
+}
+
+func modifyHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Invalid POST data", http.StatusBadRequest)
+		return
+	}
+
+	modifyId := r.FormValue("id")
+	if modifyId == "" {
+		http.Error(w, "Field \"id\" cannot be empty", http.StatusBadRequest)
+		return
+	}
+	newModifier, err := strconv.Atoi(r.FormValue("modifier"))
+	if err != nil {
+		http.Error(w, "Invalid modifier received", http.StatusBadRequest)
+		return
+	} else if newModifier != 0 && newModifier != 1 {
+		http.Error(w, "Modifier has to be an integer 0 or 1", http.StatusBadRequest)
+		return
+	}
+
+	entries, err := getEntries()
+	if err != nil {
+		http.Error(w, "Failed to read ToDos file", http.StatusInternalServerError)
+		return
+	}
+
+	newEntries, err := changeModifier(entries, modifyId, newModifier)
+	if err != nil {
+		http.Error(w, "Failed to change modifier of this entry", http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(newEntries)
+	if err != nil {
+		http.Error(w, "Failed to Marshal response body", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.Write(response)
+}
+
+func main() {
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/add", addHandler)
+	http.HandleFunc("/remove", removeHandler)
+	http.HandleFunc("/modify", modifyHandler)
+
+	fmt.Printf("\033[32mServer open, listening on port 10004\033[0m\n")
+	log.Fatal(http.ListenAndServe(":10004", nil))
+}
---
+++ static/main.js
@@ -0,0 +1,112 @@
+$(".add").on('click', function() {
+    let title = $("#todo-title").val()
+    let color = $("#todo-color").val()
+    $.ajax({
+        url: "/add",
+        type: "POST",
+        data: {
+            title: title,
+            color: color,
+        },
+        success: function(response) {
+            $("#todos").append(`
+                <div class="item" data-id="${response.id}" data-modifier="${response.modifier}" data-color="${response.color}">
+                    <p>${response.title}</p>
+                    <button class="remove">X</button>
+                </div>
+            `);
+            $("#todo-title").val("");
+            colorize();
+        },
+        error: function(response) {
+            alert(response);
+        }
+    });
+});
+
+$(document).on('click', '.remove', function() {
+    let id = $(this).parent().attr('data-id')
+        $.ajax({
+            url: "/remove",
+            type: "POST",
+            data: {
+                id: id,
+            },
+            success: function(response) {
+                let html = "";
+                response.forEach(entry => {
+                    html += `
+                    <div class="item" data-id="${entry.id}" data-modifier="${entry.modifier}" data-color="${entry.color}">
+                        <p>${entry.title}</p>
+                        <button class="remove">X</button>
+                    </div>
+                `});
+                $("#todos").html(html);
+                colorize();
+            },
+            error: function(response) {
+                alert("chuj cos nie poszlo");
+            }
+        });
+});
+
+$(document).ready(function() {
+    colorize();
+    $('#todo-color').on('input', function() {
+      const hue = $(this).val();
+      const hsl = `hsl(${hue}, 60%, 60%)`;
+      const hslBackground = `hsl(${hue}, 20%, 30%)`;
+      $("#input button").css({
+        'color': hsl,
+        'border': `2px solid ${hsl}`,
+        'background-color': hslBackground,
+    });
+    });
+  });
+
+$(document).on('click', '.item p', function() {
+    let entry = $(this).parent();
+    let entryId = entry.attr("data-id");
+    let modifier = entry.attr("data-modifier");
+    newModifier = modifier * -1 + 1;
+    entry.attr("data-modifier", newModifier);
+    $.ajax({
+        url: "/modify",
+        type: "POST",
+        data: {
+            id: entryId,
+            modifier: newModifier,
+        },
+        success: function(response) {
+            let html = "";
+            response.forEach(entry => {
+                console.log(html)
+                html += `
+                <div class="item" data-id="${entry.id}" data-modifier="${entry.modifier}" data-color="${entry.color}">
+                    <p>${entry.title}</p>
+                    <button class="remove">X</button>
+                </div>
+            `});
+            $("#todos").html(html);
+            colorize();
+        },
+        error: function(response) {
+            alert(response);
+        }
+    });
+})
+
+function colorize() {
+    $(".item").each(function() {
+        const hue = $(this).attr("data-color");
+        const hsl = `hsl(${hue}, 60%, 60%)`;
+        const hslBackground = `hsl(${hue}, 20%, 30%)`;
+        $(this).css({
+            'border': `2px solid ${hsl}`,
+            'background-color': hslBackground,
+        });
+        $(this).children().each(function() {
+            $(this).css('color', hsl);
+        })
+    })
+}
